fe233d603e8226541b4ad80b14da1af395944c33
tuan0342 <79151156+tuan0342@users.noreply.github.com>
2023-11-30 15:36:20 +0700

    feat: call api personal info

--- lib/pages/authenticated/personal_page.dart
+++ lib/pages/authenticated/personal_page.dart
@@ -1,4 +1,7 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:facebook_app/models/profile_model.dart';
 import 'package:facebook_app/my_widgets/my_app_bar.dart';
+import 'package:facebook_app/services/user_service.dart';
 import 'package:flutter/material.dart';
 
 class PersonalPage extends StatefulWidget {
@@ -9,6 +12,22 @@ class PersonalPage extends StatefulWidget {
 }
 
 class _PersonalPageState extends State<PersonalPage> {
+  late Profile profile = Profile(id: "", username: "", created: "", description: "", 
+              avatar: "", imageCover: "", link: "", address: "", city: "", country: "", listing: "", 
+              isFriend: "", online: "", coins: "");
+
+  @override
+  void initState() {
+    super.initState();
+    getProfile();
+  }
+
+  void getProfile() async {
+    final profile_ = await UserService().getProfile(context: context);
+    setState(() {
+      profile = profile_;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,26 +48,87 @@ class _PersonalPageState extends State<PersonalPage> {
                   // color: Colors.amber,
                 ),
                 Positioned(
-                  child: Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: 200,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        fit: BoxFit.fill,
-                        image: NetworkImage("https://mcdn.coolmate.me/image/October2023/nhan-vat-doraemon-3012_329.jpg"),
+                  child: CachedNetworkImage(
+                    imageUrl: profile.imageCover,
+                    imageBuilder: (context, imageProvider) => Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 200,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        image: DecorationImage(image: imageProvider, fit: BoxFit.contain),
+                        // border: Border.all(width: 1)
                       ),
                     ),
-                  ),  
-                ),
-                const Positioned(
-                  top: 130,
-                  left: 10,
-                  child: CircleAvatar(
-                    radius: 50.0,
-                    backgroundImage: NetworkImage(
-                        'https://mcdn.coolmate.me/image/October2023/nhan-vat-doraemon-3012_329.jpg'),
+                    placeholder: (context, url) => Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 200,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        image: DecorationImage(
+                          image: AssetImage("assets/images/male_default_avatar.jpeg"),
+                          fit: BoxFit.cover
+                        )
+                      ),
+                    ),
+                    errorWidget: (context, url, error) => Container(
+                      height: 200,
+                      width: MediaQuery.of(context).size.width,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.rectangle, 
+                        image: DecorationImage(
+                        image: AssetImage("assets/images/male_default_avatar.jpeg"),
+                        fit: BoxFit.cover)
+                      ),
+                    )
                   ),
                 ),
+                // const Positioned(
+                //   top: 130,
+                //   left: 10,
+                //   child: CircleAvatar(
+                //     radius: 50.0,
+                //     backgroundImage: NetworkImage(
+                //         'https://mcdn.coolmate.me/image/October2023/nhan-vat-doraemon-3012_329.jpg'),
+                //   ),
+                // ),
+                Positioned(
+                  top: 130,
+                  left: 10,
+                  child: CachedNetworkImage(
+                    imageUrl: profile.avatar,
+                    imageBuilder: (context, imageProvider) => Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(image: imageProvider, fit: BoxFit.contain),
+                        border: Border.all(width: 1)
+                      ),
+                    ),
+                    placeholder: (context, url) => Container(
+                      width: 100,
+                      height: 100,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          image: AssetImage("assets/images/male_default_avatar.jpeg"),
+                          fit: BoxFit.cover
+                        )
+                      ),
+                    ),
+                    errorWidget: (context, url, error) => Container(
+                      height: 100,
+                      width: 100,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle, 
+                        image: DecorationImage(
+                          image: AssetImage("assets/images/male_default_avatar.jpeg"),
+                          fit: BoxFit.cover
+                        )
+                      ),
+                    )
+                  ),
+                )
               ],
             ),
 
